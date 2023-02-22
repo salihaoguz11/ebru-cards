0004e0a0e9b29cca700041b8a0b33f37c426595c
Saliha <salihaoguz2011@gmail.com>
2023-02-22 22:23:47 +0000

    done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,26 @@
 import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
 function Cards(props) {
-  const { img, name } = props;
+  const { img, name, desc } = props;
+  const [showImage, setShowImage] = useState(true);
+  const handleClick = () => setShowImage(!showImage);
   return (
-    <Card className="rounded-2 m-auto player-card ebru-card">
-      <Card.Img variant="top" src={img} className="images" />
-      <Card.Body>
+    <Card
+      onClick={handleClick}
+      className="rounded-2 m-auto player-card ebru-card"
+    >
+      {showImage ? (
+        <Card.Img variant="top" src={img} className="images" />
+      ) : (
+        <ul className="m-auto">
+          <li className="h-5 mt-5 list-unstyled list">{desc}</li>
+        </ul>
+      )}
+
+      <Card.Footer>
         <Card.Title className="text-center text-primary">{name}</Card.Title>
-      </Card.Body>
+      </Card.Footer>
     </Card>
   );
 }
--- src/data.js
+++ src/data.js
@@ -2,49 +2,62 @@ export const data = [
   {
     name: "Kervan",
     img: "http://4.bp.blogspot.com/-dgeOO_8eb9A/TqpNXhv5FlI/AAAAAAAAAgw/G5pP1rijSfA/s1600/garip+ay+kervan.jpg",
+    desc: "Marbled paper or Ebru  is an art form that was developed in Central Asia and Ottoman Empire in the fifteenth century.In 2014 Ebru art was inscribed in the UNESCO's List of Representative List of the Intangible Cultural Heritage of Humanity.",
   },
   {
     name: "Birds",
     img: "https://1.bp.blogspot.com/-eSA81fq0a4U/XjDLNreXMnI/AAAAAAAACvw/7qXyPJZ49xkZ0y_AyS2IhbY4089hke5fQCLcBGAsYHQ/s1600/kus%25CC%25A7lar.jpg     ",
+    desc: "One of the most famous Ebru artist is  Garip Ay has gained worldwide fame by adopting Ebru, an ancient technique that allows him to create stunning paintings on water. Believed to have originated in Central Asia in the 13th century, Ebru made its way to Turkey by the 16th century, where it was perfected. ",
   },
   {
     name: "Van Gogh",
     img: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL25vdy1kYmQyY2Q1ZC01MDE0LTRmMzEtYjAwYi05NjM0YzE3ZTgyMjEtMTIxMC02ODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0=      ",
+    desc: " Artist Garip Ay uses a traditional Turkish art style called Ebru to paint Van Gogh's Starry Night on water. The art style consists of colorful paint being sprinkled onto oily water. The design can then be transferred to paper. ",
   },
   {
     name: "Camel",
     img: "https://i.ytimg.com/vi/W65VZJJRDeI/maxresdefault.jpg ",
+    desc: "Today mass-produced marbled paper is used for many decorational purposes, though the art of marbling continues.",
   },
   {
     name: "Violet",
     img: "https://www.allaboutturkey.com/images/ebru.jpg    ",
+    desc: " It's a very popular art in Turkey, there are many artists and art houses still producing traditional Ebru, especially in Istanbul.",
   },
   {
     name: "Peacock",
     img: "https://i.pinimg.com/736x/37/48/d3/3748d37f078b047199dd24fc1f2b8c15--ebru-art-surface-pattern.jpg      ",
+    desc: "In order to perform the Ebru art a shallow tray is filled with water and some viscous material such as gum and the gall fluid of cattle.This viscous liquid makes the colors floating on the water. Various mineral and vegetable dyes are sprinkled and carefully applied to the surface of the water with a small brush",
   },
   {
     name: "Hydrangea",
     img: "http://www.artvecraft.com/stores/1809202020415802.jpg     ",
+    desc: "After the invention of hatip designs, there existed also primitive flower designs in ebru. In 20th century, Necmeddin Okyay has developed flower designs to a degree that flowers such as tulips, carnations, poppies, roses etc. were represented as close to their natural form with marbling techniques. After making a stem and leaves, flowers are made. ",
   },
   {
     name: "Tulip in Istanbul",
     img: "https://indigodergisi.com/wp-content/uploads/2015/12/atilla_can_ebru_portre_kardelen_ataturk_istanbul_11.jpg      ",
+    desc: "Depending on the creativity of the artist, drops of negative colors are put on floating colors. or the already existing and floating dyes in the water are gently manipulated with a thin brush or by blowing on them directly or through a straw.",
   },
   {
     name: "Butterfly",
     img: "http://www.gulsefika.com/wp-content/uploads/2020/12/Ebru-Art-7-e1606834069244.jpg    ",
+    desc: " The process is repeated until the surface of the water is covered with unique and unrepeatable patterns, flower designs, circles, etc.A blank sheet of paper is laid on the water for few minutes so the floating colors are transferred to the paper.Than the paper is carefully removed from the tray and naturally dried.",
   },
   {
     name: "Ebru Art with Husn-i Hat",
     img: "https://web.itu.edu.tr/~aksind/Personal/ebru/ebru11.jpg    ",
+    desc: "What was Ebru used for? Ebru, a UNESCO-heritage listed art, was primarily used for book covers, calligraphy pieces, panels, and miniatures. Now it is used for many other decorative purposes, such as on wood, silk fabric and plastic.",
   },
+
   {
     name: "Comb",
     img: "https://i0.wp.com/resimhocam.com/wp-content/uploads/2017/02/ebru.jpg?fit=1366%2C910&ssl=1   ",
+    desc: "This pattern is obtained by applying a comb or rake perpendicularly over a gel git pattern. Nonpareil, Peacock, American are variations of combed designs. On a Nonpareil, we can apply gel git, shawl or freestyle designs with a thin awl.",
   },
   {
     name: "Rose with little bird",
     img: "https://i.pinimg.com/564x/97/2b/be/972bbeeab5f7d38a31ce5e3b8746d37e--meaningful-lyrics-ebru-art.jpg      ",
+    desc: "Ebru painting is, in fact, quite a difficult and time-consuming practice and you will need a bunch of essential ebru painting materials: a tray with prepared water, special brushes, marbling paint, marbling paper to transmit your painting, and some extra tools to create designs.",
   },
 ];
