44dfc4d42bb8bf368e5bd3a275082dc2e3dff7bb
Saliha <salihaoguz2011@gmail.com>
2023-02-22 20:42:41 +0000

    components

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const data = [
+  {
+    name: "Kervan",
+    img: "http://4.bp.blogspot.com/-dgeOO_8eb9A/TqpNXhv5FlI/AAAAAAAAAgw/G5pP1rijSfA/s1600/garip+ay+kervan.jpg",
+  },
+  {
+    name: "Birds",
+    img: "https://1.bp.blogspot.com/-eSA81fq0a4U/XjDLNreXMnI/AAAAAAAACvw/7qXyPJZ49xkZ0y_AyS2IhbY4089hke5fQCLcBGAsYHQ/s1600/kus%25CC%25A7lar.jpg     ",
+  },
+  {
+    name: "Van Gogh",
+    img: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL25vdy1kYmQyY2Q1ZC01MDE0LTRmMzEtYjAwYi05NjM0YzE3ZTgyMjEtMTIxMC02ODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo4Mjh9fX0=      ",
+  },
+  {
+    name: "Camel",
+    img: "https://i.ytimg.com/vi/W65VZJJRDeI/maxresdefault.jpg ",
+  },
+  {
+    name: "Violet",
+    img: "https://www.allaboutturkey.com/images/ebru.jpg    ",
+  },
+  {
+    name: "Peacock",
+    img: "https://i.pinimg.com/736x/37/48/d3/3748d37f078b047199dd24fc1f2b8c15--ebru-art-surface-pattern.jpg      ",
+  },
+  {
+    name: "Hydrangea",
+    img: "http://www.artvecraft.com/stores/1809202020415802.jpg     ",
+  },
+  {
+    name: "Tulip in Istanbul",
+    img: "https://indigodergisi.com/wp-content/uploads/2015/12/atilla_can_ebru_portre_kardelen_ataturk_istanbul_11.jpg      ",
+  },
+  {
+    name: "Butterfly",
+    img: "http://www.gulsefika.com/wp-content/uploads/2020/12/Ebru-Art-7-e1606834069244.jpg    ",
+  },
+  {
+    name: "Ebru Art with Husn-i Hat",
+    img: "https://web.itu.edu.tr/~aksind/Personal/ebru/ebru11.jpg    ",
+  },
+  {
+    name: "Comb",
+    img: "https://i0.wp.com/resimhocam.com/wp-content/uploads/2017/02/ebru.jpg?fit=1366%2C910&ssl=1   ",
+  },
+  {
+    name: "Rose with little bird",
+    img: "https://i.pinimg.com/564x/97/2b/be/972bbeeab5f7d38a31ce5e3b8746d37e--meaningful-lyrics-ebru-art.jpg      ",
+  },
+];
